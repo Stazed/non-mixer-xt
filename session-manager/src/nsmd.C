97d12322f52b22d43b4e333810672ee294e32ca2
Jonathan Moore Liles <j.liles@unix.net>
2021-01-02 13:25:47 -0800

    Revert "Merge remote-tracking branch 'diovudau/master'"
    This reverts commit bbe8386499621c8e60eebb06d80e966f13a69a95, reversing
    changes made to d958df0486c7397c243f5ac36bf4acbc461a1e50.
    
    Change was poorly thought out, unnecessary, ill advised, and poorly implemented, but was accepted in the name of diplomacy. That was a mistake. Reverting.

--- session-manager/src/nsmd.C
+++ session-manager/src/nsmd.C
@@ -2137,8 +2137,6 @@ announce_gui( const char *url, bool is_reply )
     else
         osc_server->send( gui_addr, "/nsm/gui/server_announce", "hi" );
 
-    osc_server->send( gui_addr, "/nsm/gui/session/root", session_root );
-
     for ( std::list<Client*>::iterator i = client.begin();
           i != client.end();
           ++i )
