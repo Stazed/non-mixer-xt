f3a1370e162bd99f8cac16494716155595185789
Stazed <stazed@manjo-mapson.com>
2021-12-23 19:48:31 -0600

    Rename "Bool" to "BBool" which name clashes with "Bool" from Xlib.h.

--- mixer/src/lv2/atom-forge.h
+++ mixer/src/lv2/atom-forge.h
@@ -111,7 +111,7 @@ typedef struct {
 	LV2_Atom_Forge_Frame* stack;
 
 	LV2_URID Blank LV2_ATOM_FORGE_DEPRECATED;
-	LV2_URID Bool;
+	LV2_URID BBool;
 	LV2_URID Chunk;
 	LV2_URID Double;
 	LV2_URID Float;
@@ -144,7 +144,7 @@ lv2_atom_forge_init(LV2_Atom_Forge* forge, const LV2_URID_Map* map)
 {
 	lv2_atom_forge_set_buffer(forge, NULL, 0);
 	forge->Blank    = map->map(map->handle, LV2_ATOM__Blank);
-	forge->Bool     = map->map(map->handle, LV2_ATOM__Bool);
+	forge->BBool    = map->map(map->handle, LV2_ATOM__Bool);
 	forge->Chunk    = map->map(map->handle, LV2_ATOM__Chunk);
 	forge->Double   = map->map(map->handle, LV2_ATOM__Double);
 	forge->Float    = map->map(map->handle, LV2_ATOM__Float);
@@ -405,7 +405,7 @@ lv2_atom_forge_double(LV2_Atom_Forge* forge, double val)
 static inline LV2_Atom_Forge_Ref
 lv2_atom_forge_bool(LV2_Atom_Forge* forge, bool val)
 {
-	const LV2_Atom_Bool a = { { sizeof(int32_t), forge->Bool }, val ? 1 : 0 };
+	const LV2_Atom_Bool a = { { sizeof(int32_t), forge->BBool }, val ? 1 : 0 };
 	return lv2_atom_forge_primitive(forge, &a.atom);
 }
 
