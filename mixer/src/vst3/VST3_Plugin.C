4dae91176f6aded3b2a24733cf9966d72c859b76
Stazed <stazed@manjo-mapson.com>
2023-12-20 21:25:48 -0600

    Add open and close_descripter functions.

--- mixer/src/vst3/VST3_Plugin.C
+++ mixer/src/vst3/VST3_Plugin.C
@@ -25,10 +25,20 @@
 
 #ifdef VST3_SUPPORT
 
+#include <filesystem>
+#include <dlfcn.h>      // dlopen, dlerror, dlsym
+
 #include "VST3_Plugin.H"
 
+using namespace Steinberg;
+
 VST3_Plugin::VST3_Plugin() :
-    Plugin_Module()
+    Plugin_Module(),
+    m_module(nullptr),
+    m_component(nullptr),
+    m_controller(nullptr),
+    m_unitInfos(nullptr),
+    _plugin_filename()
 {
     _plug_type = VST3;
 
@@ -43,6 +53,21 @@ VST3_Plugin::~VST3_Plugin()
 bool
 VST3_Plugin::load_plugin ( Module::Picked picked )
 {
+    _plugin_filename = picked.s_unique_id;
+    
+    if ( ! std::filesystem::exists(_plugin_filename) )
+    {
+        // FIXME check different location
+        WARNING("Failed to find a suitable VST3 bundle binary %s", _plugin_filename.c_str());
+        return false;
+    }
+
+    if ( !open_descriptor(0) )
+    {
+        DMESSAGE("Could not open descriptor %s", _plugin_filename.c_str());
+        return false;
+    }
+
     return false;
 }
 
@@ -128,6 +153,270 @@ void
 VST3_Plugin::handlePluginUIResized(const uint width, const uint height)
 {
     
+}
+
+// File loader.
+bool
+VST3_Plugin::open ( const std::string& sFilename )
+{
+    DMESSAGE("Open %s", sFilename.c_str());
+
+    m_module = ::dlopen(sFilename.c_str(), RTLD_LOCAL | RTLD_LAZY);
+    if (!m_module)
+        return false;
+
+    typedef bool (*VST3_ModuleEntry)(void *);
+    const VST3_ModuleEntry module_entry
+            = VST3_ModuleEntry(::dlsym(m_module, "ModuleEntry"));
+
+    if (module_entry)
+        module_entry(m_module);
+
+    return true;
+}
+
+bool
+VST3_Plugin::open_descriptor(unsigned long iIndex)
+{
+    close_descriptor();
+    
+    if (!open(_plugin_filename))
+    {
+        DMESSAGE("Could not open %s", _plugin_filename.c_str());
+        return false;
+    }
+    #if 0
+    typedef bool (PLUGIN_API *VST3_ModuleEntry)(void *);
+    const VST3_ModuleEntry module_entry
+            = reinterpret_cast<VST3_ModuleEntry> (m_pFile->resolve("ModuleEntry"));
+    if (module_entry)
+            module_entry(m_pFile->module());
+    #endif
+    typedef IPluginFactory *(PLUGIN_API *VST3_GetPluginFactory)();
+    const VST3_GetPluginFactory get_plugin_factory
+            = reinterpret_cast<VST3_GetPluginFactory> (::dlsym(m_module, "GetPluginFactory"));
+    //	= reinterpret_cast<VST3_GetPluginFactory> (m_pFile->resolve("GetPluginFactory"));
+    if (!get_plugin_factory)
+    {
+        DMESSAGE("[%p]::open(\"%s\", %lu)"
+                " *** Failed to resolve plug-in factory.", this,
+                _plugin_filename.c_str(), iIndex);
+
+        return false;
+    }
+
+    IPluginFactory *factory = get_plugin_factory();
+    if (!factory)
+    {
+        DMESSAGE("[%p]::open(\"%s\", %lu)"
+                " *** Failed to retrieve plug-in factory.", this,
+                _plugin_filename.c_str(), iIndex);
+
+        return false;
+    }
+
+    PFactoryInfo factoryInfo;
+    if (factory->getFactoryInfo(&factoryInfo) != kResultOk)
+    {
+        DMESSAGE("qtractorVst3PluginType::Impl[%p]::open(\"%s\", %lu)"
+                " *** Failed to retrieve plug-in factory information.", this,
+                _plugin_filename.c_str(), iIndex);
+
+        return false;
+    }
+
+    IPluginFactory2 *factory2 = FUnknownPtr<IPluginFactory2> (factory);
+    IPluginFactory3 *factory3 = FUnknownPtr<IPluginFactory3> (factory);
+    #if 0
+    if (factory3)
+    {
+        factory3->setHostContext(g_hostContext.get());   // FIXME
+    }
+    #endif
+    const int32 nclasses = factory->countClasses();
+
+    unsigned long i = 0;
+
+    for (int32 n = 0; n < nclasses; ++n)
+    {
+        PClassInfo classInfo;
+        if (factory->getClassInfo(n, &classInfo) != kResultOk)
+            continue;
+
+        if (::strcmp(classInfo.category, kVstAudioEffectClass))
+            continue;
+
+        if (iIndex == i)
+        {
+#if 0
+            PClassInfoW classInfoW;
+            if (factory3 && factory3->getClassInfoUnicode(n, &classInfoW) == kResultOk) {
+                    m_sName = fromTChar(classInfoW.name);
+                    m_sCategory = QString::fromLocal8Bit(classInfoW.category);
+                    m_sSubCategories = QString::fromLocal8Bit(classInfoW.subCategories);
+                    m_sVendor = fromTChar(classInfoW.vendor);
+                    m_sVersion = fromTChar(classInfoW.version);
+                    m_sSdkVersion = fromTChar(classInfoW.sdkVersion);
+            } else {
+                    PClassInfo2 classInfo2;
+                    if (factory2 && factory2->getClassInfo2(n, &classInfo2) == kResultOk) {
+                            m_sName = QString::fromLocal8Bit(classInfo2.name);
+                            m_sCategory = QString::fromLocal8Bit(classInfo2.category);
+                            m_sSubCategories = QString::fromLocal8Bit(classInfo2.subCategories);
+                            m_sVendor = QString::fromLocal8Bit(classInfo2.vendor);
+                            m_sVersion = QString::fromLocal8Bit(classInfo2.version);
+                            m_sSdkVersion = QString::fromLocal8Bit(classInfo2.sdkVersion);
+                    } else {
+                            m_sName = QString::fromLocal8Bit(classInfo.name);
+                            m_sCategory = QString::fromLocal8Bit(classInfo.category);
+                            m_sSubCategories.clear();
+                            m_sVendor.clear();
+                            m_sVersion.clear();
+                            m_sSdkVersion.clear();
+                    }
+            }
+
+            if (m_sVendor.isEmpty())
+                    m_sVendor = QString::fromLocal8Bit(factoryInfo.vendor);
+            if (m_sEmail.isEmpty())
+                    m_sEmail = QString::fromLocal8Bit(factoryInfo.email);
+            if (m_sUrl.isEmpty())
+                    m_sUrl = QString::fromLocal8Bit(factoryInfo.url);
+
+            m_iUniqueID = qHash(QByteArray(classInfo.cid, sizeof(TUID)));
+#endif
+            Vst::IComponent *component = nullptr;
+            if (factory->createInstance(
+                    classInfo.cid, Vst::IComponent::iid,
+                    (void **) &component) != kResultOk)
+            {
+                DMESSAGE("[%p]::open(\"%s\", %lu)"
+                        " *** Failed to create plug-in component.", this,
+                        _plugin_filename.c_str(), iIndex);
+
+                return false;
+            }
+
+            m_component = owned(component);
+#if 0   // FIXME
+            if (m_component->initialize(g_hostContext.get()) != kResultOk)
+            {
+                DMESSAGE("[%p]::open(\"%s\", %lu)"
+                        " *** Failed to initialize plug-in component.", this,
+                        _plugin_filename.c_str(), iIndex);
+
+                close();
+                return false;
+            }
+#endif
+            Vst::IEditController *controller = nullptr;
+            if (m_component->queryInterface(
+                    Vst::IEditController::iid,
+                    (void **) &controller) != kResultOk)
+            {
+                TUID controller_cid;
+                if (m_component->getControllerClassId(controller_cid) == kResultOk)
+                {
+                    if (factory->createInstance(
+                            controller_cid, Vst::IEditController::iid,
+                            (void **) &controller) != kResultOk)
+                    {
+                        DMESSAGE("qtractorVst3PluginType::Impl[%p]::open(\"%s\", %lu)"
+                                " *** Failed to create plug-in controller.", this,
+                                _plugin_filename.c_str(), iIndex);
+
+                    }
+#if 0   // FIXME
+                    if (controller &&
+                            controller->initialize(g_hostContext.get()) != kResultOk)
+                    {
+                        DMESSAGE("[%p]::open(\"%s\", %lu)"
+                                " *** Failed to initialize plug-in controller.", this,
+                                _plugin_filename.c_str(), iIndex);
+
+                        controller = nullptr;
+                    }
+#endif
+                }
+            }
+
+            if (controller) m_controller = owned(controller);
+
+            Vst::IUnitInfo *unitInfos = nullptr;
+            if (m_component->queryInterface(
+                            Vst::IUnitInfo::iid,
+                            (void **) &unitInfos) != kResultOk)
+            {
+                if (m_controller &&
+                        m_controller->queryInterface(
+                                Vst::IUnitInfo::iid,
+                                (void **) &unitInfos) != kResultOk)
+                {
+                    DMESSAGE("qtractorVst3PluginType::Impl[%p]::open(\"%s\", %lu)"
+                            " *** Failed to create plug-in units information.", this,
+                            _plugin_filename.c_str(), iIndex);
+                }
+            }
+
+            if (unitInfos) m_unitInfos = owned(unitInfos);
+
+            // Connect components...
+            if (m_component && m_controller)
+            {
+                FUnknownPtr<Vst::IConnectionPoint> component_cp(m_component);
+                FUnknownPtr<Vst::IConnectionPoint> controller_cp(m_controller);
+                if (component_cp && controller_cp)
+                {
+                        component_cp->connect(controller_cp);
+                        controller_cp->connect(component_cp);
+                }
+            }
+
+            return true;
+        }
+
+        ++i;
+    }
+
+    return false;
+}
+
+void
+VST3_Plugin::close_descriptor()
+{
+    if (m_component && m_controller)
+    {
+        FUnknownPtr<Vst::IConnectionPoint> component_cp(m_component);
+        FUnknownPtr<Vst::IConnectionPoint> controller_cp(m_controller);
+        if (component_cp && controller_cp)
+        {
+            component_cp->disconnect(controller_cp);
+            controller_cp->disconnect(component_cp);
+        }
+    }
+
+    m_unitInfos = nullptr;
+
+    if (m_component && m_controller &&
+            FUnknownPtr<Vst::IEditController> (m_component).getInterface())
+    {
+        m_controller->terminate();
+    }
+
+    m_controller = nullptr;
+
+    if (m_component)
+    {
+        m_component->terminate();
+        m_component = nullptr;
+        typedef bool (PLUGIN_API *VST3_ModuleExit)();
+        const VST3_ModuleExit module_exit
+                = reinterpret_cast<VST3_ModuleExit> (::dlsym(m_module, "ModuleExit"));
+              //  = reinterpret_cast<VST3_ModuleExit> (m_pFile->resolve("ModuleExit"));
+
+        if (module_exit)
+            module_exit();
+    }
 }
 
 void
