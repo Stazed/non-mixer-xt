2213f829b64349e33c49380fa4061cdaf77b8300
Stazed <stazed@manjo-mapson.com>
2024-01-13 08:04:30 -0600

    Get open_descriptor() working.

--- mixer/src/vst2/Vst2_Discovery.C
+++ mixer/src/vst2/Vst2_Discovery.C
@@ -215,14 +215,26 @@ bool qtractor_vst2_scan::open ( const std::string& sFilename )
 // Plugin loader.
 bool qtractor_vst2_scan::open_descriptor ( unsigned long iIndex )
 {
-	if (m_pLibrary == nullptr)
-		return false;
+    if (m_pLibrary == nullptr)
+        return false;
 
-	close_descriptor();
+    close_descriptor();
 
-#ifdef CONFIG_DEBUG_0
-	qDebug("qtractor_vst2_scan[%p]::open_descriptor(%lu)", this, iIndex);
-#endif
+    DMESSAGE("open_descriptor - iIndex = (%lu)",  iIndex);
+
+    VST_GetPluginInstance pfnGetPluginInstance = lib_symbol<VST_GetPluginInstance>(m_pLibrary, "VSTPluginMain");
+
+    if (pfnGetPluginInstance == nullptr)
+        pfnGetPluginInstance = lib_symbol<VST_GetPluginInstance> (m_pLibrary, "main");
+
+    if (pfnGetPluginInstance == nullptr)
+    {
+        DMESSAGE("error", "Not a VST plugin");
+        return false;
+    }
+
+    m_pEffect = (*pfnGetPluginInstance)(qtractor_vst2_scan_callback);
+
 
 #if 0       // FIXME
 	VST_GetPluginInstance pfnGetPluginInstance
@@ -238,46 +250,61 @@ bool qtractor_vst2_scan::open_descriptor ( unsigned long iIndex )
 	m_pEffect = (*pfnGetPluginInstance)(qtractor_vst2_scan_callback);
         
 #endif
-	if (m_pEffect == nullptr) {
-	#ifdef CONFIG_DEBUG
-		qDebug("qtractor_vst2_scan[%p]: plugin instance could not be created.", this);
-	#endif
-		return false;
+    if (m_pEffect == nullptr)
+    {
+        DMESSAGE("plugin instance could not be created.");
+        return false;
+    }
+
+    // Did VST plugin instantiated OK?
+    if (m_pEffect->magic != kEffectMagic)
+    {
+        DMESSAGE("Plugin is not a valid VST.");
+        m_pEffect = nullptr;
+        return false;
+    }
+
+    // Check whether it's a VST Shell...
+    const int categ = vst2_dispatch(effGetPlugCategory, 0, 0, nullptr, 0.0f);
+    if (categ == kPlugCategShell)
+    {
+        int id = 0;
+        char buf[40];
+        unsigned long i = 0;
+        for ( ; iIndex >= i; ++i)
+        {
+            buf[0] = (char) 0;
+            id = vst2_dispatch(effShellGetNextPlugin, 0, 0, (void *) buf, 0.0f);
+            if (id == 0 || !buf[0])
+                break;
+        }
+        // Check if we're actually the intended plugin...
+        if (i < iIndex || id == 0 || !buf[0]) {
+        #ifdef CONFIG_DEBUG
+                qDebug("qtractor_vst2_scan[%p]: "
+                        "vst2_shell(%lu) plugin is not a valid VST.", this, iIndex);
+        #endif
+                m_pEffect = nullptr;
+                return false;
+        }
+        // Make it known...
+        g_iVst2ShellCurrentId = id;
+        // Re-allocate the thing all over again...
+
+        pfnGetPluginInstance = lib_symbol<VST_GetPluginInstance>(m_pLibrary, "VSTPluginMain");
+        
+        if (pfnGetPluginInstance == nullptr)
+            pfnGetPluginInstance = lib_symbol<VST_GetPluginInstance> (m_pLibrary, "main");
+       
+
+        if (pfnGetPluginInstance == nullptr)
+        {
+            DMESSAGE("error", "Not a VST plugin");
+            m_pEffect = nullptr;
+            return false;
 	}
 
-	// Did VST plugin instantiated OK?
-	if (m_pEffect->magic != kEffectMagic) {
-	#ifdef CONFIG_DEBUG
-		qDebug("qtractor_vst2_scan[%p]: plugin is not a valid VST.", this);
-	#endif
-		m_pEffect = nullptr;
-		return false;
-	}
-
-	// Check whether it's a VST Shell...
-	const int categ = vst2_dispatch(effGetPlugCategory, 0, 0, nullptr, 0.0f);
-	if (categ == kPlugCategShell) {
-		int id = 0;
-		char buf[40];
-		unsigned long i = 0;
-		for ( ; iIndex >= i; ++i) {
-			buf[0] = (char) 0;
-			id = vst2_dispatch(effShellGetNextPlugin, 0, 0, (void *) buf, 0.0f);
-			if (id == 0 || !buf[0])
-				break;
-		}
-		// Check if we're actually the intended plugin...
-		if (i < iIndex || id == 0 || !buf[0]) {
-		#ifdef CONFIG_DEBUG
-			qDebug("qtractor_vst2_scan[%p]: "
-				"vst2_shell(%lu) plugin is not a valid VST.", this, iIndex);
-		#endif
-			m_pEffect = nullptr;
-			return false;
-		}
-		// Make it known...
-		g_iVst2ShellCurrentId = id;
-		// Re-allocate the thing all over again...
+        m_pEffect = (*pfnGetPluginInstance)(qtractor_vst2_scan_callback);
                 
 #if 0   // FIXME
 		pfnGetPluginInstance
@@ -296,62 +323,62 @@ bool qtractor_vst2_scan::open_descriptor ( unsigned long iIndex )
 		m_pEffect = (*pfnGetPluginInstance)(qtractor_vst2_scan_callback);
                 
 #endif
-		// Not needed anymore, hopefully...
-		g_iVst2ShellCurrentId = 0;
-		// Don't go further if failed...
-		if (m_pEffect == nullptr) {
-		#ifdef CONFIG_DEBUG
-			qDebug("qtractor_vst2_scan[%p]: "
-				"vst2_shell(%lu) plugin instance could not be created.", this, iIndex);
-		#endif
-			return false;
-		}
-		if (m_pEffect->magic != kEffectMagic) {
-		#ifdef CONFIG_DEBUG
-			qDebug("qtractor_vst2_scan[%p]: "
-				"vst2_shell(%lu) plugin is not a valid VST.", this, iIndex);
-		#endif
-			m_pEffect = nullptr;
-			return false;
-		}
-	#ifdef CONFIG_DEBUG
-		qDebug("qtractor_vst2_scan[%p]: "
-			"vst2_shell(%lu) id=0x%x name=\"%s\"", this, i, id, buf);
-	#endif
-	}
-	else
-	// Not a VST Shell plugin...
-	if (iIndex > 0) {
-		m_pEffect = nullptr;
-		return false;
-	}
+            // Not needed anymore, hopefully...
+            g_iVst2ShellCurrentId = 0;
+            // Don't go further if failed...
+            if (m_pEffect == nullptr) {
+            #ifdef CONFIG_DEBUG
+                    qDebug("qtractor_vst2_scan[%p]: "
+                            "vst2_shell(%lu) plugin instance could not be created.", this, iIndex);
+            #endif
+                    return false;
+            }
+            if (m_pEffect->magic != kEffectMagic) {
+            #ifdef CONFIG_DEBUG
+                    qDebug("qtractor_vst2_scan[%p]: "
+                            "vst2_shell(%lu) plugin is not a valid VST.", this, iIndex);
+            #endif
+                    m_pEffect = nullptr;
+                    return false;
+            }
+    #ifdef CONFIG_DEBUG
+            qDebug("qtractor_vst2_scan[%p]: "
+                    "vst2_shell(%lu) id=0x%x name=\"%s\"", this, i, id, buf);
+    #endif
+    }
+    else
+    // Not a VST Shell plugin...
+    if (iIndex > 0) {
+            m_pEffect = nullptr;
+            return false;
+    }
 
 //	vst2_dispatch(effIdentify, 0, 0, nullptr, 0.0f);
-	vst2_dispatch(effOpen,     0, 0, nullptr, 0.0f);
+    vst2_dispatch(effOpen,     0, 0, nullptr, 0.0f);
 
-	// Get label name...
-	char szName[256]; ::memset(szName, 0, sizeof(szName));
-	if (vst2_dispatch(effGetEffectName, 0, 0, (void *) szName, 0.0f))
-		m_sName = szName;
+    // Get label name...
+    char szName[256]; ::memset(szName, 0, sizeof(szName));
+    if (vst2_dispatch(effGetEffectName, 0, 0, (void *) szName, 0.0f))
+            m_sName = szName;
 
-	// Specific inquiries...
-	m_iFlagsEx = 0;
+    // Specific inquiries...
+    m_iFlagsEx = 0;
 
-	if (vst2_canDo("sendVstEvents"))       m_iFlagsEx |= effFlagsExCanSendVstEvents;
-	if (vst2_canDo("sendVstMidiEvent"))    m_iFlagsEx |= effFlagsExCanSendVstMidiEvents;
-	if (vst2_canDo("sendVstTimeInfo"))     m_iFlagsEx |= effFlagsExCanSendVstTimeInfo;
-	if (vst2_canDo("receiveVstEvents"))    m_iFlagsEx |= effFlagsExCanReceiveVstEvents;
-	if (vst2_canDo("receiveVstMidiEvent")) m_iFlagsEx |= effFlagsExCanReceiveVstMidiEvents;
-	if (vst2_canDo("receiveVstTimeInfo"))  m_iFlagsEx |= effFlagsExCanReceiveVstTimeInfo;
-	if (vst2_canDo("offline"))             m_iFlagsEx |= effFlagsExCanProcessOffline;
-	if (vst2_canDo("plugAsChannelInsert")) m_iFlagsEx |= effFlagsExCanUseAsInsert;
-	if (vst2_canDo("plugAsSend"))          m_iFlagsEx |= effFlagsExCanUseAsSend;
-	if (vst2_canDo("mixDryWet"))           m_iFlagsEx |= effFlagsExCanMixDryWet;
-	if (vst2_canDo("midiProgramNames"))    m_iFlagsEx |= effFlagsExCanMidiProgramNames;
+    if (vst2_canDo("sendVstEvents"))       m_iFlagsEx |= effFlagsExCanSendVstEvents;
+    if (vst2_canDo("sendVstMidiEvent"))    m_iFlagsEx |= effFlagsExCanSendVstMidiEvents;
+    if (vst2_canDo("sendVstTimeInfo"))     m_iFlagsEx |= effFlagsExCanSendVstTimeInfo;
+    if (vst2_canDo("receiveVstEvents"))    m_iFlagsEx |= effFlagsExCanReceiveVstEvents;
+    if (vst2_canDo("receiveVstMidiEvent")) m_iFlagsEx |= effFlagsExCanReceiveVstMidiEvents;
+    if (vst2_canDo("receiveVstTimeInfo"))  m_iFlagsEx |= effFlagsExCanReceiveVstTimeInfo;
+    if (vst2_canDo("offline"))             m_iFlagsEx |= effFlagsExCanProcessOffline;
+    if (vst2_canDo("plugAsChannelInsert")) m_iFlagsEx |= effFlagsExCanUseAsInsert;
+    if (vst2_canDo("plugAsSend"))          m_iFlagsEx |= effFlagsExCanUseAsSend;
+    if (vst2_canDo("mixDryWet"))           m_iFlagsEx |= effFlagsExCanMixDryWet;
+    if (vst2_canDo("midiProgramNames"))    m_iFlagsEx |= effFlagsExCanMidiProgramNames;
 
-	m_bEditor = (m_pEffect->flags & effFlagsHasEditor);
+    m_bEditor = (m_pEffect->flags & effFlagsHasEditor);
 
-	return true;
+    return true;
 }
 
 
@@ -377,31 +404,32 @@ void qtractor_vst2_scan::close_descriptor (void)
 // File unloader.
 void qtractor_vst2_scan::close (void)
 {
-	if (m_pLibrary == nullptr)
-		return;
+    if (m_pLibrary == nullptr)
+        return;
 
-#ifdef CONFIG_DEBUG_0
-	qDebug("qtractor_vst2_scan[%p]::close()", this);
-#endif
+    DMESSAGE("close()");
 
-	vst2_dispatch(effClose, 0, 0, 0, 0.0f);
+    vst2_dispatch(effClose, 0, 0, 0, 0.0f);
 #if 0   // FIXME
-	if (m_pLibrary->isLoaded() && !m_bEditor)
-		m_pLibrary->unload();
+    if (m_pLibrary->isLoaded() && !m_bEditor)
+            m_pLibrary->unload();
 
-	delete m_pLibrary;
+    delete m_pLibrary;
 #endif
-	m_pLibrary = nullptr;
+    lib_close(m_pLibrary);
+    m_pLibrary = nullptr;
 
-	m_bEditor = false;
+    m_bEditor = false;
 }
 
 
 // Check wether plugin is loaded.
 bool qtractor_vst2_scan::isOpen (void) const
 {
-	if (m_pLibrary == nullptr)
-		return false;
+    if (m_pLibrary == nullptr)
+        return false;
+    
+    return true;
 #if 0   // FIXME
 	return m_pLibrary->isLoaded();
 #endif
@@ -501,12 +529,47 @@ static VstIntPtr VSTCALLBACK qtractor_vst2_scan_callback ( AEffect* effect,
 void vst2_discovery_scan_file ( const std::string& sFilename, std::list<Plugin_Module::Plugin_Info> & vst2pr )
 {
 #ifdef CONFIG_DEBUG
-        qDebug("qtractor_vst2_scan_file(\"%s\")", sFilename.toUtf8().constData());
+    qDebug("qtractor_vst2_scan_file(\"%s\")", sFilename.toUtf8().constData());
 #endif
-        qtractor_vst2_scan plugin;
+    qtractor_vst2_scan plugin;
 
-        if (!plugin.open(sFilename))
-                return;
+    if (!plugin.open(sFilename))
+            return;
+
+    unsigned long i = 0;
+    while (plugin.open_descriptor(i))
+    {
+        Plugin_Module::Plugin_Info pi("VST2");
+
+        pi.name = plugin.name();
+#if 0
+        pi.author = plugin.vendor();
+
+        if (std::strstr(plugin.subCategory().c_str(), "Instrument") != nullptr)
+        {
+            pi.category =  "Instrument Plugin";
+        }
+        else
+        {
+             pi.category = getCategoryFromName(pi.name);
+        }
+#endif
+        pi.audio_inputs = plugin.numInputs();
+        pi.audio_outputs = plugin.numOutputs();
+
+        pi.s_unique_id = plugin.uniqueID();
+        pi.plug_path = sFilename;
+
+        vst2pr.push_back(pi);
+
+        DMESSAGE("name = %s: category = %s: ID = %s: PATH = %s",
+                pi.name.c_str(), pi.category.c_str(), pi.s_unique_id.c_str(), pi.plug_path.c_str());
+
+        plugin.close_descriptor();
+        ++i;
+    }
+
+    plugin.close();
 #if 0
         QTextStream sout(stdout);
         unsigned long i = 0;
